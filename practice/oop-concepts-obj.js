93b5cb5c24591b99efb1e485b94833274f2c1346
Lisa Zolotarova <35108960+lisazolotarova@users.noreply.github.com>
2021-11-29 02:29:05 +0200

    Update oop-concepts-obj.js

--- practice/oop-concepts-obj.js
+++ practice/oop-concepts-obj.js
@@ -1,46 +1,86 @@
 // Inheritance
+// Example with __proto__
 let scienceCrew = {
-    field: neurobiology,
-    education: "Ph.D",
-    researchInfo() {
-        alert("Biology of human behavior")
-    }
-  };
-  
-  let scientist1 = {
-    name: Mark,
-    age: 29,
-    __proto__: scienceCrew
-  };
-  
-  let scientist2 = {
-    name: Ben,
-    age: 35,
-    experience: "5 years",
-    __proto__: scienceCrew
-  };
-  
-  scientist1.researchInfo();
-  alert(scientist2.experience); 
+  field: neurobiology,
+  education: "Ph.D",
+  researchInfo() {
+    alert("Biology of human behavior");
+  },
+};
 
-// Encapsulation
-class Customer {
-    constructor(name, id) {
-        this.name = name;
-        this.id = id;
-    }
+let scientist1 = {
+  name: Mark,
+  age: 29,
+  __proto__: scienceCrew,
+  researchInfo() {
+    alert("Biology of human");
+  },
+};
 
-    add_Review(comment) {
-        this.comment = comment;
-    }
+let scientist2 = {
+  name: Ben,
+  age: 35,
+  experience: "5 years",
+  __proto__: scienceCrew,
+};
 
-    getReviewDetails() {
-        console.log(`User ${this.nickname}, Product feedback: ${this.comment}`);
-    }
+scientist1.researchInfo();
+alert(scientist2.experience);
+
+// Example with Classes
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayName() {
+    return 'Hey there, I am ' + this.name;
+  }
 }
- 
-let customer1 = new customer('Ron', 34678);
-customer1.add_Review('Not bad');
-customer1.getReviewDetails();
+
+class Men extends Person {
+  constructor(name) {
+    super(name)
+  }
+}
+class RandomGuy extends Person {
+  constructor(name) {
+    super(name)
+  }
+}
+
+const dylan = new Men('Dylan');
+const mark = new RandomGuy('Mark');
+
+console.log(dylan.sayName()) 
+console.log(mark.sayName())
+
+
+// Encapsulation - TBC
+
 
 // Polymorphism
+class Animal {
+  constructor(species, lifetime) {
+    this.species = species;
+    this.lifetime = lifetime;
+  }
+}
+
+class Pet extends Animal {
+  constructor(species, lifetime, breed) {
+    super(species, lifetime);
+    this.breed = breed;
+  }
+
+  addInfo() {
+    console.log("Danger level for human: low")
+  } 
+}
+
+let wildBeing = new Animal("cougar", "15-20 years");
+console.log(wildBeing);
+
+let fluffyFriend = new Pet("dog", "12-15 years", "rottweiler");
+console.log(fluffyFriend);
+console.log(fluffyFriend.addInfo);
