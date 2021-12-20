2912722cb6b5b63565c6332bdea75f45f6318c90
Lisa Zolotarova <35108960+lisazolotarova@users.noreply.github.com>
2021-12-20 12:19:29 +0200

    Update oop-concepts-obj.js

--- practice/oop-concepts-obj.js
+++ practice/oop-concepts-obj.js
@@ -1,33 +1,33 @@
 // Inheritance
-// Example with __proto__
-let scienceCrew = {
-  field: neurobiology,
-  education: "Ph.D",
-  researchInfo() {
-    alert("Biology of human behavior");
-  },
-};
+// 1 UPDATED
+function Food() {}
+function Bread() {}
 
-let scientist1 = {
-  name: Mark,
-  age: 29,
-  __proto__: scienceCrew,
-  researchInfo() {
-    alert("Biology of human");
-  },
-};
+let basicFood = new Food();
 
-let scientist2 = {
-  name: Ben,
-  age: 35,
-  experience: "5 years",
-  __proto__: scienceCrew,
-};
+Bread.prototype = basicFood;
 
-scientist1.researchInfo();
-alert(scientist2.experience);
+let bread = new Bread();
+bread.constructor == Food;
 
-// Example with Classes
+Bread.prototype.constructor = Bread;
+bread = new Bread();
+bread.constructor == Bread;
+
+// 2 UPDATED
+function junkFood() {
+  this.name = "snacks";
+}
+
+function chips() {
+  junkFood.call(this);
+}
+
+chips.prototype = Object.create(junkFood.prototype);
+const pringles = new chips();
+console.log(pringles.name);
+
+// 3 
 class Person {
   constructor(name) {
     this.name = name;
@@ -80,28 +80,68 @@ alert(person.getName()); // Another name
 
 
 
-// Polymorphism
-class Animal {
-  constructor(species, lifetime) {
-    this.species = species;
-    this.lifetime = lifetime;
-  }
-}
-
-class Pet extends Animal {
-  constructor(species, lifetime, breed) {
-    super(species, lifetime);
+// Polymorphism UPDATED
+class DogBreed {
+  constructor(age, breed) {
+    this.age = age;
     this.breed = breed;
   }
-
-  addInfo() {
-    console.log("Danger level for human: low")
-  } 
 }
 
-let wildBeing = new Animal("cougar", "15-20 years");
-console.log(wildBeing);
+class Labrador extends DogBreed {
+  constructor(age, breed, color) {
+    super(age, breed);
+    this.color = color;
+  }
 
-let fluffyFriend = new Pet("dog", "12-15 years", "rottweiler");
-console.log(fluffyFriend);
-console.log(fluffyFriend.addInfo);
+  showDogInfo() {
+    console.log(
+      "Age is " +
+        this.age +
+        " the breed is " +
+        this.breed +
+        " color is " +
+        this.color
+    );
+  }
+}
+
+class Staff extends DogBreed {
+  constructor(age, breed, color) {
+    super(age, breed);
+    this.color = color;
+  }
+
+  showDogInfo() {
+    console.log(
+      "Age is " +
+        this.age +
+        " the breed is " +
+        this.breed +
+        " color is " +
+        this.color
+    );
+  }
+}
+
+class Terrier extends DogBreed {
+  constructor(age, breed, color) {
+    super(age, breed);
+    this.color = color;
+  }
+
+  showDogInfo() {
+    console.log(
+      "Age is " +
+        this.age +
+        " the breed is " +
+        this.breed +
+        " color is " +
+        this.color
+    );
+  }
+}
+
+let labrador = new Labrador("5", "Labrador", "brown");
+var staff = new Staff("10", "Staff", "brown and white");
+var terrier = new Terrier("4", "Terrier", "grey");
