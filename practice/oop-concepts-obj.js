3d689e50ee61e3efc23a15e56220bf5bbe7e13fa
Lisa Zolotarova <lisa.zolotarova@gmail.com>
2022-01-06 15:11:06 +0200

    files updates

--- practice/oop-concepts-obj.js
+++ practice/oop-concepts-obj.js
@@ -1,46 +1,147 @@
 // Inheritance
-let scienceCrew = {
-    field: neurobiology,
-    education: "Ph.D",
-    researchInfo() {
-        alert("Biology of human behavior")
-    }
-  };
-  
-  let scientist1 = {
-    name: Mark,
-    age: 29,
-    __proto__: scienceCrew
-  };
-  
-  let scientist2 = {
-    name: Ben,
-    age: 35,
-    experience: "5 years",
-    __proto__: scienceCrew
-  };
-  
-  scientist1.researchInfo();
-  alert(scientist2.experience); 
+// 1 UPDATED
+function Food() {}
+function Bread() {}
+
+let basicFood = new Food();
+
+Bread.prototype = basicFood;
+
+let bread = new Bread();
+bread.constructor == Food;
+
+Bread.prototype.constructor = Bread;
+bread = new Bread();
+bread.constructor == Bread;
+
+// 2 UPDATED
+function junkFood() {
+  this.name = "snacks";
+}
+
+function chips() {
+  junkFood.call(this);
+}
+
+chips.prototype = Object.create(junkFood.prototype);
+const pringles = new chips();
+console.log(pringles.name);
+
+// 3 
+class Person {
+  constructor(name) {
+    this.name = name;
+  }
+
+  sayName() {
+    return 'Hey there, I am ' + this.name;
+  }
+}
+
+class Men extends Person {
+  constructor(name) {
+    super(name)
+  }
+}
+class RandomGuy extends Person {
+  constructor(name) {
+    super(name)
+  }
+}
+
+const dylan = new Men('Dylan');
+const mark = new RandomGuy('Mark');
+
+console.log(dylan.sayName()) 
+console.log(mark.sayName())
+
 
 // Encapsulation
-class Customer {
-    constructor(name, id) {
-        this.name = name;
-        this.id = id;
+let person = {
+  name: "Default name",
+  setName: function (value) {
+    let exp = new RegExp(/\d+/);
+    if (exp.test(value)) {
+      alert("Invalid Name");
+    } else {
+      this.name = value;
     }
+  },
+  getName: function () {
+    return this.name;
+  },
+};
 
-    add_Review(comment) {
-        this.comment = comment;
-    }
+alert(person.getName()); // Default name
+person.setName("Another name");
+alert(person.getName()); // Another name
+person.setName(24); // Invalid Name
+alert(person.getName()); // Another name
 
-    getReviewDetails() {
-        console.log(`User ${this.nickname}, Product feedback: ${this.comment}`);
-    }
+
+
+// Polymorphism UPDATED
+class DogBreed {
+  constructor(age, breed) {
+    this.age = age;
+    this.breed = breed;
+  }
 }
- 
-let customer1 = new customer('Ron', 34678);
-customer1.add_Review('Not bad');
-customer1.getReviewDetails();
 
-// Polymorphism
+class Labrador extends DogBreed {
+  constructor(age, breed, color) {
+    super(age, breed);
+    this.color = color;
+  }
+
+  showDogInfo() {
+    console.log(
+      "Age is " +
+        this.age +
+        " the breed is " +
+        this.breed +
+        " color is " +
+        this.color
+    );
+  }
+}
+
+class Staff extends DogBreed {
+  constructor(age, breed, color) {
+    super(age, breed);
+    this.color = color;
+  }
+
+  showDogInfo() {
+    console.log(
+      "Age is " +
+        this.age +
+        " the breed is " +
+        this.breed +
+        " color is " +
+        this.color
+    );
+  }
+}
+
+class Terrier extends DogBreed {
+  constructor(age, breed, color) {
+    super(age, breed);
+    this.color = color;
+  }
+
+  showDogInfo() {
+    console.log(
+      "Age is " +
+        this.age +
+        " the breed is " +
+        this.breed +
+        " color is " +
+        this.color
+    );
+  }
+}
+
+let labrador = new Labrador("5", "Labrador", "brown");
+var staff = new Staff("10", "Staff", "brown and white");
+var terrier = new Terrier("4", "Terrier", "grey");
